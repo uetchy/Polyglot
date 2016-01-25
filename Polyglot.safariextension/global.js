df833d14740f5c6e889fccb55e0af4b660b249e4
Yasuaki Uechi <uetchy@randompaper.co>
2016-01-25 13:07:16 +0900

    Use switch

--- Polyglot.safariextension/global.js
+++ Polyglot.safariextension/global.js
@@ -5,46 +5,47 @@ safari.application.addEventListener('message', handleMessage, false);
 
 // Perform context menu commands
 function performCommand(event) {
-  if (event.command === 'contextMenuTranslate') {
-    safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('getSelectedText');
+  switch (event.command) {
+    case 'contextMenuTranslate':
+      safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('getSelectedText');
+      break;
   }
 }
 
 // Handle each message by name
 function handleMessage(msg) {
-  console.log(window.top);
-  if (msg.name === 'finishedGetSelectedText') {
-    console.log(msg.message);
-    var apiKey = safari.extension.secureSettings.apiKey;
-    var targetLanguage = safari.extension.settings.targetLanguage;
+  switch(msg.name) {
+    case 'finishedGetSelectedText':
+      var apiKey = safari.extension.secureSettings.apiKey;
+      var targetLanguage = safari.extension.settings.targetLanguage;
 
-    if (msg.message === '') {
-      console.log("Select message first");
-      return;
-    } else if (apiKey === '') {
-      console.log("Set api key");
-      return;
-    } else if (targetLanguage === '') {
-      console.log("Set target language");
-      return;
-    }
+      if (msg.message === '') {
+        return;
+      } else if (apiKey === '') {
+        console.log("Set api key");
+        return;
+      } else if (targetLanguage === '') {
+        console.log("Set target language");
+        return;
+      }
 
-    request
-      .get('https://www.googleapis.com/language/translate/v2')
-      .query({
-        key: apiKey,
-        target: targetLanguage,
-        q: msg.message
-      })
-      .set('Accept', 'application/json')
-      .end(function(err, res) {
-        console.log(res);
-        var translations = res.body.data.translations;
-        for (var t of translations) {
-          console.log(t.translatedText, t.detectedSourceLanguage);
-        }
-        safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('showPanel', translations);
-      });
+      request
+        .get('https://www.googleapis.com/language/translate/v2')
+        .query({
+          key: apiKey,
+          target: targetLanguage,
+          q: msg.message
+        })
+        .set('Accept', 'application/json')
+        .end(function(err, res) {
+          console.log(res);
+          var translations = res.body.data.translations;
+          for (var t of translations) {
+            console.log(t.translatedText, t.detectedSourceLanguage);
+          }
+          safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('showPanel', translations);
+        });
+
   }
 }
 
