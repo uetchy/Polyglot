973677530a30c618d8df3de5424b815176b3a337
Yasuaki Uechi <uetchy@randompaper.co>
2016-01-25 11:37:38 +0900

    Add translation code

--- Polyglot.safariextension/global.js
+++ Polyglot.safariextension/global.js
@@ -1,4 +1,4 @@
-console.log("main.js");
+var request = window.superagent;
 
 safari.application.addEventListener('command', performCommand, false);
 safari.application.addEventListener('message', handleMessage, false);
@@ -10,24 +10,40 @@ function performCommand(event) {
 }
 
 function handleMessage(msg) {
-  // if (window == window.top)
+  console.log(window.top);
   if (msg.name === 'finishedGetSelectedText') {
     console.log(msg.message);
+    var apiKey = safari.extension.secureSettings.apiKey;
+    var targetLanguage = safari.extension.settings.targetLanguage;
+
+    if (msg.message === '') {
+      console.log("Select message first");
+      return;
+    } else if (apiKey === '') {
+      console.log("Set api key");
+      return;
+    } else if (targetLanguage === '') {
+      console.log("Set target language");
+      return;
+    }
+
+    request
+      .get('https://www.googleapis.com/language/translate/v2')
+      .query({
+        key: apiKey,
+        target: targetLanguage,
+        q: msg.message
+      })
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        console.log(res);
+        var translations = res.body.data.translations;
+        for (var t of translations) {
+          console.log(t.translatedText, t.detectedSourceLanguage);
+        }
+        safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('showPanel', translations);
+      });
   }
 }
 
 // safari.application.addEventListener("popover", popoverHandler, true);
-//
-// function popoverHandler(event){
-//     apiUserName = safari.extension.settings.apiUserName;
-//     apiPassword = safari.extension.settings.apiPassword;
-//
-//     $('#add-style').remove();
-//     if (safari.extension.settings.ConohaMode){
-//          $('head link:last').after('<link id="add-style" rel="stylesheet" href="panel_conoha-mode.css">');
-//     }else{
-//         $('body').css("background-color","#FFFFFF");
-//     }
-//     showLoading();
-//     GetAllServers();
-// };
--- Polyglot.safariextension/injected.js
+++ Polyglot.safariextension/injected.js
@@ -1,8 +1,61 @@
 safari.self.addEventListener('message', handleMessage, false);
+document.addEventListener('onmouseup', handleMouseUp, false);
 
+// Get selected text and dispatch to global script
 function handleMessage(msg) {
   if (msg.name === 'getSelectedText') {
     var sel = window.getSelection().toString();
     safari.self.tab.dispatchMessage('finishedGetSelectedText', sel);
+  } else if (msg.name === 'showPanel') {
+    showPanel(msg);
   }
+}
+
+function handleMouseUp(e) {
+
+}
+
+// Show panel with given text
+function showPanel(msg) {
+  var translations = msg.message;
+  var coords = getSelectionCoords();
+  console.log(coords.x + ", " + coords.y);
+  var el = document.createElement('div');
+  for (var t of translations) {
+    el.innerHTML += t.translatedText + '\n';
+  }
+  el.style.position = 'absolute';
+  el.style.padding = '20px';
+  el.style.background = '#EFEFEF';
+  el.style.borderRadius = '6px';
+  el.style.minWidth = '200px';
+  el.style.minHeight = '100px';
+  el.style.left = coords.x + 'px';
+  el.style.top = (coords.y + document.body.scrollTop) + 'px';
+  document.body.insertBefore(el, document.body.firstChild);
+}
+
+// Return selection coords
+function getSelectionCoords(win) {
+  win = win || window;
+  var doc = win.document;
+  var x = 0,
+    y = 0;
+  sel = win.getSelection();
+  if (sel.rangeCount) {
+    range = sel.getRangeAt(0).cloneRange();
+    if (range.getClientRects) {
+      range.collapse(true);
+      rects = range.getClientRects();
+      if (rects.length > 0) {
+        rect = rects[0];
+      }
+      x = rect.left;
+      y = rect.bottom;
+    }
+  }
+  return {
+    x: x,
+    y: y
+  };
 }
