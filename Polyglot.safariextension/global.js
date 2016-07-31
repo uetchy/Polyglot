a39bd16f326a9ce35ec46e97de34f7d658dfc754
Yasuaki Uechi <uetchy@randompaper.co>
2016-07-31 13:30:50 +0900

    👉 Add xo linter

--- Polyglot.safariextension/global.js
+++ Polyglot.safariextension/global.js
@@ -1,9 +1,9 @@
 import request from 'superagent';
 
 // Get settings
-var apiKey = safari.extension.secureSettings.apiKey;
-var targetLanguage = safari.extension.settings.targetLanguage;
-var keyboardShortcut = safari.extension.settings.keyboardShortcut;
+let apiKey = safari.extension.secureSettings.apiKey;
+let targetLanguage = safari.extension.settings.targetLanguage;
+let keyboardShortcut = safari.extension.settings.keyboardShortcut;
 
 // Set event handler
 safari.application.addEventListener('command', performCommand, false);
@@ -13,81 +13,88 @@ safari.extension.secureSettings.addEventListener('change', settingsChanged, fals
 
 // Perform commands from users
 function performCommand(event) {
-  switch (event.command) {
-    case 'translateSelectedText':
-      safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('getSelectedText');
-      break;
-  }
+	switch (event.command) {
+		case 'translateSelectedText':
+			safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('getSelectedText');
+			break;
+		default:
+
+	}
 }
 
 // Handle message from injected script
 function handleMessage(msg) {
-  switch (msg.name) {
-    case 'finishedGetSelectedText':
-      if (msg.message === '') {
-        return;
-      }
-      var target = msg.target;
-      target.page.dispatchMessage('showPanel', '<div class="polyglot__loader">Loading</div>');
+	switch (msg.name) {
+		case 'finishedGetSelectedText':
+			if (msg.message === '') {
+				return;
+			}
+			var target = msg.target;
+			target.page.dispatchMessage('showPanel', '<div class="polyglot__loader">Loading</div>');
 
-      if (apiKey === '') {
-        target.page.dispatchMessage('updatePanel', 'Set API key. See <a href="https://git.io/vzQ2y" target="_blank">visual guide</a>');
-        return;
-      } else if (targetLanguage === '') {
-        target.page.dispatchMessage('updatePanel', 'Set target language');
-        return;
-      }
+			if (apiKey === '') {
+				target.page.dispatchMessage('updatePanel', 'Set API key. See <a href="https://git.io/vzQ2y" target="_blank">visual guide</a>');
+				return;
+			} else if (targetLanguage === '') {
+				target.page.dispatchMessage('updatePanel', 'Set target language');
+				return;
+			}
 
-      request
-        .get('https://www.googleapis.com/language/translate/v2')
-        .query({
-          key: apiKey,
-          target: targetLanguage,
-          q: msg.message
-        })
-        .set('Accept', 'application/json')
-        .end(function(err, res) {
-          // Handle errors
-          if (res.body.error) {
-            var error = res.body.error.errors[0];
-            switch (error.reason) {
-              case 'invalid':
-                target.page.dispatchMessage('updatePanel', "Target language is invalid. please check it");
-                break;
-              case 'keyInvalid':
-                target.page.dispatchMessage('updatePanel', 'API key is invalid. please check it');
-                break;
-            }
-            return;
-          }
+			request
+				.get('https://www.googleapis.com/language/translate/v2')
+				.query({
+					key: apiKey,
+					target: targetLanguage,
+					q: msg.message
+				})
+				.set('Accept', 'application/json')
+				.end(function(err, res) {
+					// Handle errors
+					if (res.body.error) {
+						var error = res.body.error.errors[0];
+						switch (error.reason) {
+							case 'invalid':
+								target.page.dispatchMessage('updatePanel', "Target language is invalid. please check it");
+								break;
+							case 'keyInvalid':
+								target.page.dispatchMessage('updatePanel', 'API key is invalid. please check it');
+								break;
+						}
+						return;
+					}
 
-          var translations = res.body.data.translations;
-          var result = '';
-          for (var t of translations) {
-            result += t.translatedText + '<br/>';
-          }
+					const translations = res.body.data.translations;
+					let result = '';
+					for (const t of translations) {
+						result += t.translatedText + '<br/>';
+					}
 
-          target.page.dispatchMessage('updatePanel', result);
-        });
+					target.page.dispatchMessage('updatePanel', result);
+				});
 
-      break;
-    case 'requestKeyboardShortcut':
-      msg.target.page.dispatchMessage('keyboardShortcutReceived', keyboardShortcut);
-  }
+			break;
+		case 'requestKeyboardShortcut':
+			msg.target.page.dispatchMessage('keyboardShortcutReceived', keyboardShortcut);
+			break;
+		default:
+
+	}
 }
 
 // Update setting values immediately
 function settingsChanged(event) {
-  switch (event.key) {
-    case 'apiKey':
-      apiKey = event.newValue;
-      break;
-    case 'targetLanguage':
-      targetLanguage = event.newValue;
-      break;
-    case 'keyboardShortcut':
-      keyboardShortcut = event.newValue;
-      safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('keyboardShortcutReceived', keyboardShortcut);
-      break;
-  }
+	switch (event.key) {
+		case 'apiKey':
+			apiKey = event.newValue;
+			break;
+		case 'targetLanguage':
+			targetLanguage = event.newValue;
+			break;
+		case 'keyboardShortcut':
+			keyboardShortcut = event.newValue;
+			safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('keyboardShortcutReceived', keyboardShortcut);
+			break;
+		default:
+
+	}
 }
--- Polyglot.safariextension/injected.js
+++ Polyglot.safariextension/injected.js
@@ -1,120 +1,130 @@
+let isPanelOpen = false;
+let keyboardShortcut = null;
+const PANEL_ID = 'polyglot__panel';
+
 // Only initialize in a top-level page
 if (window.top === window) {
-  safari.self.addEventListener('message', handleMessage, false);
+	safari.self.addEventListener('message', handleMessage, false);
 
-  window.addEventListener('keydown', handleKeydown, false);
-  window.addEventListener('mouseup', handleMouseUp, false);
+	window.addEventListener('keydown', handleKeydown, false);
+	window.addEventListener('mouseup', handleMouseUp, false);
 
-  var isPanelOpen = false;
-  var keyboardShortcut = null;
-  var PANEL_ID = 'polyglot__panel';
-
-  safari.self.tab.dispatchMessage('requestKeyboardShortcut');
+	safari.self.tab.dispatchMessage('requestKeyboardShortcut');
 }
 
 // Get selected text and return to global script
 function handleMessage(msg) {
-  switch (msg.name) {
-    case 'keyboardShortcutReceived':
-      keyboardShortcut = parseInt(msg.message);
-      break;
-    case 'getSelectedText':
-      getSelectedText();
-      break;
-    case 'showPanel':
-      showPanel(msg.message);
-      break;
-    case 'updatePanel':
-      updatePanel(msg.message);
-      break;
-  }
+	switch (msg.name) {
+		case 'keyboardShortcutReceived':
+			keyboardShortcut = parseInt(msg.message, 10);
+			break;
+		case 'getSelectedText':
+			getSelectedText();
+			break;
+		case 'showPanel':
+			showPanel(msg.message);
+			break;
+		case 'updatePanel':
+			updatePanel(msg.message);
+			break;
+		default:
+
+	}
 }
 
 function handleMouseUp(e) {
-  let panel = document.getElementById(PANEL_ID);
+	const panel = document.getElementById(PANEL_ID);
 
-  if (isPanelOpen && !isDescendant(panel, e.target)) {
-    removePanel();
-  }
+	if (isPanelOpen && !isDescendant(panel, e.target)) {
+		removePanel();
+	}
 }
 
 function handleKeydown(e) {
-  if (e.keyCode === keyboardShortcut) {
-    e.preventDefault();
-    getSelectedText();
-  }
+	if (e.keyCode === keyboardShortcut) {
+		e.preventDefault();
+		getSelectedText();
+	}
 }
 
 function getSelectedText() {
-  let sel = window.getSelection().toString();
-  safari.self.tab.dispatchMessage('finishedGetSelectedText', sel);
+	const sel = window.getSelection().toString();
+	safari.self.tab.dispatchMessage('finishedGetSelectedText', sel);
 }
 
 function removePanel() {
-  let panel = document.getElementById(PANEL_ID);
-  panel.remove();
-  isPanelOpen = false;
+	const panel = document.getElementById(PANEL_ID);
+	panel.remove();
+	isPanelOpen = false;
 }
 
 // Show panel with given text
 function showPanel(content) {
-  if (isPanelOpen) {
-    removePanel();
-  }
-  let bounds = getSelectionBoundingRect();
-  if (bounds === null) {
-    return false;
-  }
-  let el = document.createElement('div');
-  el.innerHTML = content;
-  el.id = PANEL_ID;
-  el.style.left = bounds.left + 'px';
-  el.style.top = bounds.bottom + 'px';
-  document.body.insertBefore(el, document.body.firstChild);
-  isPanelOpen = true;
+	if (isPanelOpen) {
+		removePanel();
+	}
+	const bounds = getSelectionBoundingRect();
+	if (bounds === null) {
+		return false;
+	}
+	const el = document.createElement('div');
+	el.innerHTML = content;
+	el.id = PANEL_ID;
+	el.style.left = bounds.left + 'px';
+	el.style.top = bounds.bottom + 'px';
+	document.body.insertBefore(el, document.body.firstChild);
+	isPanelOpen = true;
 }
 
 function updatePanel(content) {
-  let el = document.getElementById(PANEL_ID);
-  el.innerHTML = content;
+	const el = document.getElementById(PANEL_ID);
+	el.innerHTML = content;
 }
 
 // Return selection coords
 function getSelectionBoundingRect() {
-  let rect = {
-    left: 0,
-    top: 0,
-    right: 0,
-    bottom: 0
-  };
+	const rect = {
+		left: 0,
+		top: 0,
+		right: 0,
+		bottom: 0
+	};
 
-  let sel = document.getSelection();
-  for (var i = 0; i < sel.rangeCount; ++i) {
-    let _rect = sel.getRangeAt(i).getBoundingClientRect();
-    if (rect.left < _rect.left) rect.left = _rect.left;
-    if (rect.top < _rect.top) rect.top = _rect.top;
-    if (rect.right < _rect.right) rect.right = _rect.right;
-    if (rect.bottom < _rect.bottom) rect.bottom = _rect.bottom;
-  }
-  rect.width = rect.right - rect.left;
-  rect.height = rect.bottom - rect.top;
-  rect.left += window.pageXOffset;
-  rect.top += window.pageYOffset;
-  rect.right += window.pageXOffset;
-  rect.bottom += window.pageYOffset;
-  return sel.rangeCount ? rect : null;
-};
+	const sel = document.getSelection();
+	for (let i = 0; i < sel.rangeCount; ++i) {
+		const _rect = sel.getRangeAt(i).getBoundingClientRect();
+		if (rect.left < _rect.left) {
+			rect.left = _rect.left;
+		}
+		if (rect.top < _rect.top) {
+			rect.top = _rect.top;
+		}
+		if (rect.right < _rect.right) {
+			rect.right = _rect.right;
+		}
+		if (rect.bottom < _rect.bottom) {
+			rect.bottom = _rect.bottom;
+		}
+	}
+	rect.width = rect.right - rect.left;
+	rect.height = rect.bottom - rect.top;
+	rect.left += window.pageXOffset;
+	rect.top += window.pageYOffset;
+	rect.right += window.pageXOffset;
+	rect.bottom += window.pageYOffset;
+	return sel.rangeCount ? rect : null;
+}
 
 function isDescendant(parent, child) {
-  if (parent === child) {
-    return true;
-  }
-  let node = child.parentNode;
-  while (node !== null) {
-    if (node === parent) {
-      return true;
-    }
-    node = node.parentNode;
-  }
-  return false;
+	if (parent === child) {
+		return true;
+	}
+	let node = child.parentNode;
+	while (node !== null) {
+		if (node === parent) {
+			return true;
+		}
+		node = node.parentNode;
+	}
+	return false;
 }
