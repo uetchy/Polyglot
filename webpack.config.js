04baf486ce15a78785364075d384e568cc09fcc8
Yasuaki Uechi <uetchy@randompaper.co>
2016-08-14 11:58:07 +0900

    style: Fix xo config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
-var path = require("path");
-var extensionPath = path.join(__dirname, "Polyglot.safariextension");
+const {resolve} = require('path');
+
+const extensionPath = resolve(__dirname, 'Polyglot.safariextension');
 
 module.exports = {
-  context: extensionPath,
-  entry: {
-    global: "./global.js",
-    injected: "./injected.js"
-  },
-  output: {
-    path: extensionPath,
-    filename: "[name].entry.js"
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015']
-      }
-    }]
-  }
+	context: extensionPath,
+	entry: {
+		global: './global.js',
+		injected: './injected.js'
+	},
+	output: {
+		path: extensionPath,
+		filename: '[name].entry.js'
+	},
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel',
+			query: {
+				presets: ['es2015']
+			}
+		}]
+	}
 };
