3821f2ea780f09af0449880a35d9c3335e8e2a2e
Yasuaki Uechi <uetchy@randompaper.co>
2017-03-29 17:18:56 +0900

    chore(deps): update webpack and safari ext

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 		loaders: [{
 			test: /\.js$/,
 			exclude: /(node_modules|bower_components)/,
-			loader: 'babel',
+			loader: 'babel-loader',
 			query: {
 				presets: ['es2015']
 			}
