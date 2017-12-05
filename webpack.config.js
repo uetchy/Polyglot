c18cfaa8fb5cc588a183d282d698bde2ff041666
Yasuaki Uechi <y@uechi.io>
2017-12-05 09:20:42 +0900

    prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const { resolve } = require('path')
+const { resolve } = require('path');
 
-const extensionPath = resolve(__dirname, 'Polyglot.safariextension')
+const extensionPath = resolve(__dirname, 'Polyglot.safariextension');
 
 module.exports = {
   context: extensionPath,
   entry: {
     global: ['./global.js'],
-    injected: ['./injected.js']
+    injected: ['./injected.js'],
   },
   output: {
     path: extensionPath,
-    filename: '[name].entry.js'
+    filename: '[name].entry.js',
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
-}
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
