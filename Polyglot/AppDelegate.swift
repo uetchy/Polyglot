f0d24eaf7cac9fe0f3b0367a04f047a31662a6e8
Yasuaki Uechi <y@uechi.io>
2018-10-26 14:20:35 +0900

    config UI

--- Polyglot/AppDelegate.swift
+++ Polyglot/AppDelegate.swift
@@ -21,10 +21,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     recordView.tintColor = NSColor(red: 0.164, green: 0.517, blue: 0.823, alpha: 1)
     let keyCombo = KeyCombo(doubledCocoaModifiers: .command)
     recordView.keyCombo = keyCombo
+    let hotKey = HotKey(identifier: "PolyglotHotkey", keyCombo: keyCombo!, target: self, action: #selector(AppDelegate.hotkeyCalled))
+    hotKey.register()
+    recordView.didChange = { keyCombo in
+      guard let keyCombo = keyCombo else { return } // Clear shortcut
+      print("keyCode: \(keyCombo.keyCode)")
+      print("modifiers: \(keyCombo.modifiers)")
+      let ud = UserDefaults.standard
+      ud.set(keyCombo.keyCode, forKey: "keyCode")
+    }
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
     // Insert code here to tear down your application
+    HotKeyCenter.shared.unregisterAll()
+  }
+  
+  @objc func hotkeyCalled() {
+    print("HotKey called!!!!")
   }
 
 
