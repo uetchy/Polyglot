49b4b06533af67fb9c74a11686ff93adb70d3c49
Yasuaki Uechi <y@uechi.io>
2019-07-07 05:25:22 +0900

    chore: tidy up old files

--- webpack.config.js
+++ webpack.config.js
@@ -11,14 +11,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-typescript'],
-          },
-        },
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
       },
     ],
   },
